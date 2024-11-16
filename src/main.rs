1779669536ca7b65eb08eeff12836e237b4ff0e7
devyk100 <devyk100@gmail.com>
2024-11-16 12:16:54 +0530

    done with the modulo inverse and modulo exponentiation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+const PRIME_MOD: u32 = 65_537;
 fn main() {
-    println!("Hello, world!");
+    let points = [(1,12), (2,10), (3,2), (4, 3)];
+    lagrange_interpolate(&points);
+    let ans = modulo_inverse(199, PRIME_MOD);
+    println!("{ans}");
 }
+
+
+fn modulo_inverse(x: u16, mod_num: u32) -> u64{
+    modulo_exponentiation(x, mod_num-2, mod_num)
+}
+
+
+fn lagrange_interpolate(points:&[(i32, i32)]){
+    let a = points[0].1;
+    println!("{a}");
+
+}
+
+
+fn modulo_exponentiation(x: u16, power: u32, mod_num: u32)-> u64{
+    let mod_num:u64 = mod_num.into();
+    let mut exp = power;
+    let mut result: u64 = 1;
+    let mut base: u64 = x.into();
+    while exp > 0 {
+        if exp % 2 == 1{
+            result = (result * base)%mod_num;
+        }
+        base %= mod_num as u64;
+        base = (base * base)%mod_num;
+        exp /= 2;
+    }
+    return result.into();
+}
+
